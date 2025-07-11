0a202e8869b56f637eea20bdb7dd8cf33abfa075
JonasBa97 <jonas.balb@outlook.de>
2025-07-11 13:47:54 +0200

    Update versionierung test for failure 1

--- versionierung.sh
+++ versionierung.sh
@@ -6,18 +6,19 @@ az acr login --name skyerededucation >> /dev/null
 
 # Hole alle Tags des Images
 latest_tag=$(az acr repository show-tags --name skyerededucation --repository lugx-gaming --output tsv | grep -v latest | sort -V | tail -n 1)
-echo "$latest_tag"
 
 if [ -z "$latest_tag" ]; then
   echo "Keine vorhandene Version gefunden. Starte mit 0.1.0"
   export new_version="0.1.0"
 else
   # Extrahiere Haupt- und Nebenversion
+  echo "latest_tag: $latest_tag"
   major=$(echo "$latest_tag" | cut -d. -f1)
   minor=$(echo "$latest_tag" | cut -d. -f2)
   revision=$(echo "$latest_tag" | cut -d. -f3)
 
   # Erhöhe die Nebenversion um 1
-  # new_minor=$((minor + 1))
+  new_minor=$((minor + 1))
   export new_version="$major.$new_minor.$revision"
 fi
+
